8655f988ef0cc0e84fe002e7862206f4ff1d913f
Rendha <rendhazuper@gmail.com>
2024-12-05 18:24:07 +0700

    ganti php native

---
+++ Backend/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "choiz";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    echo "Koneksi Gagal";
+}else{
+    echo "Koneksi Berhasil";
+}
