87aaa99567d44f8a341acf9a72dbef1434375f6b
Rendha <rendhazuper@gmail.com>
2024-12-07 09:04:14 +0700

    cekLogin

---
+++ Backend/Auth/cekLogin.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+var_dump($_SESSION);
+
+header("Access-Control-Allow-Origin: http://localhost:3000");  // Sesuaikan dengan domain frontend kamu
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    // exit;
+}
+
+
+if (!isset($_SESSION['user'])) {
+   
+    http_response_code(401); // Unauthorized
+    echo json_encode(["error" => "Harap login untuk mengakses halaman ini."]);
+    exit;
+}
+
+
+echo json_encode([
+    "status" => true,
+    "message" => "User is logged in.",
+    "username" => $_SESSION['user']['username'],
+    "level" => $_SESSION['user']['level']
+]);
